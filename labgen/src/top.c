c828a404ebf54a8c64dfc6f260e3c58915d080dd
Derriick <derriick.ensiie@yahoo.com>
2018-01-19 12:36:08 +0100

    ':' à la 1e ligne puis '|'

--- labgen/src/top.c
+++ labgen/src/top.c
@@ -192,15 +192,15 @@ int lg_gen(Tlds* ds, FILE* lstream, FILE* ystream, Cstr lcfname)
 
 	for (int i = 0; i < ds->dx; ++i)
 		for (int j = 0; j < ds->dy; ++j) {
-			char sep = '|';
+			char sep = ':';
 
 			sq = ds->squares[i][j];
 
 			if (sq.kind == LDS_OUT) {
-				fprintf(ystream, "cell_%d_%d\n\t: { return 0; }\n;\n\n", i, j);
+				fprintf(ystream, "cell_%d_%d\n\t%c { return 0; }\n;\n\n", i, j, sep);
 			}
 			else if (sq.kind != LDS_WALL) {
-				fprintf(ystream, "cell_%d_%d:\n", i, j);
+				fprintf(ystream, "cell_%d_%d\n", i, j);
 				for (int i2 = 0; i2 <= 2; ++i2)
 					for (int j2 = 0; j2 <= 2; ++j2) {
 						int x = i + i2 - 1;
@@ -213,6 +213,7 @@ int lg_gen(Tlds* ds, FILE* lstream, FILE* ystream, Cstr lcfname)
 								sq3 = ds->squares[dest.x][dest.y];
 								if(sq3.kind != LDS_WALL && (dest.x != x || dest.y != y)){
 									fprintf(ystream, "\t%c %s cell_%d_%d\n", sep, card[j2][i2], dest.x, dest.y);
+									sep = '|';
 								}
 							}
 						}
@@ -230,20 +231,21 @@ int lg_gen(Tlds* ds, FILE* lstream, FILE* ystream, Cstr lcfname)
 									Tpoint dest = sq2.sq_whd;
 
 									fprintf(ystream, "\t%c %s cell_%d_%d\n", sep, card[j2][i2], dest.x, dest.y);
+									sep = '|';
 								}
 								else if ((i2+j2)%2 == 0) {
 									if (ds->squares[x][j].kind != LDS_WALL ||
 										ds->squares[i][y].kind != LDS_WALL)
 									{
 										fprintf(ystream, "\t%c %s cell_%d_%d\n", sep, card[j2][i2], x, y);
+										sep = '|';
 									}
 								}
-								else
+								else {
 									fprintf(ystream, "\t%c %s cell_%d_%d\n", sep, card[j2][i2], x, y);
-								
-								
-							}	
-												
+									sep = '|';
+								}
+							}
 						}
 					}
 
